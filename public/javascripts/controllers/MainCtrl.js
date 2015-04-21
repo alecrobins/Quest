bc1b46326e3d9d04f2bdfc49d7b9462e97230dc7
Alec Robins <alecrobins@gmail.com>
2015-04-21 16:14:37 +0800

    Post styling done

--- public/javascripts/controllers/MainCtrl.js
+++ public/javascripts/controllers/MainCtrl.js
@@ -18,7 +18,7 @@ angular.module('app')
         date: "March 22",
         location: "Hanoi, Vietnam",
         chapter: "Test Chapter 1",
-        excerpt: "This is ia asd fa dfa sd  a sdfl;aksjdf;lajksdfla;   asldf ajsdf la sldf alsdf a;. Test ata  s d fa sdnthisi a as d  asdflkasjl;znv , .zcv. zcv. adf   2 asdf asdf. asldf ajsdf la sldf alsdf a;. Test ata  s d fa sdnthisi a as d  asdflkasjl;znv , .zcv. zcv. adf   2 asdf asdf.",
+        excerpt: "This is ia asd fa dfa sd  a sdfl;aksjdf;lajksdfla. Asldf ajsdf la sldf alsdf a;. Test ata  s d fa sdnthisi a as d  asdflkasjl;znv , .zcv. zcv. adf   2 asdf asdf.",
         url: "http://www.google.com",
         likes: 998,
         featureImage: {
@@ -39,7 +39,7 @@ angular.module('app')
         chapter: "This is the best possible chapter name adj adf ad fad fa end",
         excerpt: "This is ia asd fa dfa sd  a sdfl;aksjdf;lajksdfla;   asldf ajsdf la sldf alsdf a;.",
         url: "http://www.google.com",
-        likes: 998,
+        likes: 1318,
         featureImage: {
           url: "http://localhost:3000/assets/images/cow-filler.jpg"
         }
