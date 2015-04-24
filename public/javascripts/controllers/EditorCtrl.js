84934c945c86e86fdb40cdcca22216d48e9e80c4
Alec Robins <alecrobins@gmail.com>
2015-04-24 14:16:54 +0800

    Editor placeholder / styles working

--- public/javascripts/controllers/EditorCtrl.js
+++ public/javascripts/controllers/EditorCtrl.js
@@ -2,9 +2,19 @@
 var editorCtrl = angular.module('app')
   .controller('EditorCtrl', ['$scope', 'postService', function($scope, postService) {
 
+    $scope.test = "hello";
+
     $scope.post = {
-      test: "",
-      content: ""
-    };
+      chapter: null,
+      location: null,
+      title: null,
+      content: null
+   	};
+
+    var helloTest = $scope.post;
+
+    $scope.$watch("post.chapter", function(newValue, oldValue) {
+        console.log($scope.post);
+    });
 
 }]);
